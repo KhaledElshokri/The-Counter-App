c52d2f82e68b5d0da1baf763ed1b8b6457c236fb
khaled Elshokri <khaledelshokri28@outlook.com>
2024-09-24 17:28:52 -0400

    Missing toggle of data list only

--- app/src/main/java/com/example/helloworld/DataActivity.java
+++ app/src/main/java/com/example/helloworld/DataActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DataActivity extends AppCompatActivity {
@@ -22,10 +25,11 @@ public class DataActivity extends AppCompatActivity {
     protected TextView counterName3;
     protected TextView maxCount;
     protected SharedPreferenceHelper DataSPH;
-    protected RecyclerView eventTable;
+    protected TableLayout eventTable;
 
     protected List<String> eventDisplayList;
     protected List<String> eventList;
+    protected int toggleFlag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,7 @@ public class DataActivity extends AppCompatActivity {
         counterName2 = findViewById(R.id.view_data_count2);
         counterName3 = findViewById(R.id.view_data_count3);
         maxCount = findViewById(R.id.view_data_total);
+        eventTable = findViewById(R.id.event_table);
         DataSPH = new SharedPreferenceHelper(getApplicationContext());
 
         counterName1.setText( DataSPH.getProfileName1() + ": " + String.valueOf(DataSPH.getButtonOneCount()) + " events");
@@ -63,8 +68,46 @@ public class DataActivity extends AppCompatActivity {
         // Setting up the list for the logged Clicks
         eventList = DataSPH.getProfileNames();
 
+        Collections.reverse(eventList);
+        // Loop through the list of strings and add each as a row
+        for (String event : eventList) {
+            // Create a new TableRow
+            TableRow tableRow;
+            tableRow = new TableRow(this);
 
+            // Create a new TextView for the event string
+            TextView textView = new TextView(this);
+            if(toggleFlag == 1)
+            {
+                if (event.equals(DataSPH.getProfileName1()))
+                {
+                    textView.setText("Counter 1: ");
+                }
+                else if (event.equals(DataSPH.getProfileName2()))
+                {
+                    textView.setText("Counter 2: ");
+                }
+                else if (event.equals(DataSPH.getProfileName3()))
+                {
+                    textView.setText("Counter 3: ");
+                }
+            }
+            else
+            {
+                textView.setText(event);
+            }
 
+            textView.setPadding(16, 16, 16, 16);  // Set padding for better readability
+
+            // Add the TextView to the TableRow
+            tableRow.addView(textView);
+
+            // Optionally, you can set a background or other styling for each row
+            tableRow.setBackgroundResource(android.R.drawable.dialog_holo_light_frame); // Optional styling
+
+            // Add the TableRow to the TableLayout
+            eventTable.addView(tableRow);
+        }
     }
     // Handle "Up" button behavior
     @Override
@@ -97,21 +140,47 @@ public class DataActivity extends AppCompatActivity {
 
     private void toggleMode()
     {
-        for (String name: eventDisplayList)
+        if(toggleFlag == 0)
         {
-            if (name.equals(DataSPH.getProfileName1()))
+            toggleFlag = 1; // Setting this mode to 1
+
+            for (String name: eventDisplayList)
             {
-                counterName1.setText("Counter 1: "  + String.valueOf(DataSPH.getButtonOneCount()) + " events");
-            }
-            else if (name.equals(DataSPH.getProfileName2()))
-            {
-                counterName1.setText("Counter 2: " + String.valueOf(DataSPH.getButtonTwoCount()) + " events");
-            }
-            else if (name.equals(DataSPH.getProfileName3()))
-            {
-                counterName1.setText("Counter 3: " + String.valueOf(DataSPH.getButtonThreeCount()) + " events");
+                if (name.equals(DataSPH.getProfileName1()))
+                {
+                    counterName1.setText("Counter 1: "  + String.valueOf(DataSPH.getButtonOneCount()) + " events");
+                }
+                else if (name.equals(DataSPH.getProfileName2()))
+                {
+                    counterName2.setText("Counter 2: " + String.valueOf(DataSPH.getButtonTwoCount()) + " events");
+                }
+                else if (name.equals(DataSPH.getProfileName3()))
+                {
+                    counterName3.setText("Counter 3: " + String.valueOf(DataSPH.getButtonThreeCount()) + " events");
+                }
             }
         }
+        else
+        {
+            toggleFlag = 0;
+
+            for (String name: eventDisplayList)
+            {
+                if (name.equals(DataSPH.getProfileName1()))
+                {
+                    counterName1.setText(DataSPH.getProfileName1() + ": " + String.valueOf(DataSPH.getButtonOneCount()) + " events");
+                }
+                else if (name.equals(DataSPH.getProfileName2()))
+                {
+                    counterName2.setText(DataSPH.getProfileName2()  + ": " + String.valueOf(DataSPH.getButtonTwoCount()) + " events");
+                }
+                else if (name.equals(DataSPH.getProfileName3()))
+                {
+                    counterName3.setText(DataSPH.getProfileName3() + ": " + String.valueOf(DataSPH.getButtonThreeCount()) + " events");
+                }
+            }
+        }
+
     }
 }
 
